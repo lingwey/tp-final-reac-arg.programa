f2f2afefa4f67f9fcfc90aa35073b9eb9d25b59c
ignacio zanetti <nachozanetti0@gmail.com>
2023-11-29 16:52:20 -0300

    soluciona errores en el fomulario

--- src/componentes/form/Registro.jsx
+++ src/componentes/form/Registro.jsx
@@ -1,45 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './estilosForm.css';
 
 export const Registro = () => {
-    const controlSubmit = (e) => {
-        e.preventDefault();
-        console.log('envidado')
-    }
+    
+    const {register, handleSubmit} = useForm()
 
-    const [nombre, setNombre] = useState("")
-    const [apellido, setApellido] = useState("")
-    const [email, setEmail] = useState("")
-    const [contrasenia, setcontrasenia] = useState("")
-    const [contrasenia2, setcontrasenia2] = useState("")
-    useEffect(
-        
-    )
+    const enviar = (data) =>{
+        console.log(data)
+    }
 
   return (
     <div className="contenedor">
-        <form onSubmit={controlSubmit}>
+        <form  onSubmit={handleSubmit(enviar)}>
             <div>
                 <h2>Registrarse</h2>
                 <div>
                     <label htmlFor="nombre">nombre</label>
-                    <input type="text"  placeholder="nombre" id="nombre"/>
+                    <input type="text"  placeholder="nombre" id="nombre" {...register("nombre")}/>
                 </div>
                 <div>
                     <label htmlFor="apellido">apellido</label>
-                    <input type="text"  placeholder="apellido" id="apellido"/>
+                    <input type="text"  placeholder="apellido" id="apellido" {...register("apellido")}/>
                 </div>
                 <div>
                     <label htmlFor="mail">mail</label>
-                    <input type="email"  placeholder="mail" id="mail"/>
+                    <input type="email"  placeholder="mail" id="mail" {...register("mail")}/>
+                </div>
+                <div>
+                    <label htmlFor="telefono">telefono</label>
+                    <input type="phone"  placeholder="1111" id="telefono" {...register("telefono")}/>
                 </div>
                 <div>
                     <label htmlFor="contrasenia">contraseña</label>
-                    <input type="password"  placeholder="contraseña" id="contrasenia"/>
+                    <input type="password"  placeholder="contraseña" id="contrasenia" {...register("contrasenia")}/>
                 </div>
                 <div>
                     <label htmlFor="contrasenia2">repitir contraseña</label>
-                    <input type="password"  placeholder="repita contraseña" id="contrasenia2"/>
+                    <input type="password"  placeholder="repita contraseña" id="contrasenia2" {...register("contrasenia2")}/>
                 </div>
               <button type="submit">enviar</button>
             </div>
